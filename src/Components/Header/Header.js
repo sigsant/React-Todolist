38d00ca14ff1f41407a8e6403baca0905488a666
sigsant <sigfridsantamera@hotmail.com>
2021-03-09 00:07:02 +0100

    Version inicial

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = (props) => {
+    return(
+        <header class="container">
+            <h2 class="text-center m-5"> React-Agenda </h2>
+        </header>
+    )
+}
+
+export default Header;
