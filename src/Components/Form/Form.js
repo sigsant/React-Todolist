f161b2090abc00daaa296ca9a631aff123aaf9d8
sigsant <sigfridsantamera@hotmail.com>
2021-03-12 23:12:28 +0100

    Actualizar css en input de tareas

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
-import {TextField, Container} from '@material-ui/core';
+import {TextField, Container, Button} from '@material-ui/core';
+import {v4 as uuidv4} from 'uuid';
 
 import './Form.css';
 
 const Form = () => {
 
-    const onClickHello = () => console.log('Click me again!')
+    useEffect(() => {
+        console.table(task);
+    })
+
+    const [task, setTask] = useState([
+        {tarea: "Estudiar Angular", completado: false, id: uuidv4()}, 
+        {tarea: "Escuchar podcasts", completado: false, id: uuidv4()}, 
+        {tarea: "Hacer Ejercicio", completado: false, id: uuidv4()}
+    ]);
+
+    const addElement = (e) => {
+        const [name, value] = e.target;
+        console.log(`Nombre: ${name} -- Valor ${value}`);
+    }
+
 
     return(
         <Container maxWidth="sm">
             <form noValidate autoComplete="off">
-                <div id="inputField">
-                <TextField id="tarea" label="Nombre de la tarea"/>
-                </div>
-                <div id="iconField">
-                    <AddCircleIcon id="iconAdd" onClick={onClickHello}/>
-                </div>
+                
+                    <TextField id="tarea" 
+                    name="tarea" 
+                    label="Nombre de la tarea" 
+                    placeholder="Nombre de la tarea"
+                    onChange={addElement}/>
+
+                    <Button type="submit" 
+                            variant="contained"
+                            color="primary"
+                            size="medium"
+                            startIcon={<AddCircleIcon />}
+                            id="iconAdd">
+                            Añadir
+                    </Button>
             </form>
         </Container>
     )
